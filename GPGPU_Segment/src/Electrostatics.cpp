c84304596a1be10b3e60ea384202e2b06bf8a128
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2021-05-16 20:08:20 -0500

    GPGPU_Segment: Electrostatics.cpp: Convert to kernel coding style
    Convert Electrostatics.cpp to kernel coding syle, and fix the line
    endings to unix style.

--- GPGPU_Segment/src/Electrostatics.cpp
+++ GPGPU_Segment/src/Electrostatics.cpp
@@ -6,35 +6,34 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * ElectroMag is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  *  along with ElectroMag.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "CL_Electrostatics.hpp"
+#include <iostream>
 
 using electro::pointCharge;
-
-#include <iostream>
 using std::cout;
 using std::endl;
-void TestCL(Vector3<Array<float> >& fieldLines,
-            Array<pointCharge<float> >& pointCharges,
-            size_t n, float resolution,  perfPacket& perfData,
-            bool useCurvature)
+
+void TestCL(Vector3<Array<float> > &fieldLines,
+	    Array<pointCharge<float> > &pointCharges, size_t n,
+	    float resolution, perfPacket &perfData, bool useCurvature)
 {
-    CLElectrosFunctor<float>::BindDataParams dataParams
-        = {&fieldLines, &pointCharges, n, resolution, perfData, useCurvature};
-    cout<<"TestCL: Binding data"<<endl;
-    CLtest.BindData((void*) &dataParams);
-    cout<<"TestCL: Starting run"<<endl;
-    CLtest.Run();
-    cout<<"TestCL: done"<<endl;
+	CLElectrosFunctor<float>::BindDataParams dataParams = {
+		&fieldLines, &pointCharges, n,
+		resolution,  perfData,	    useCurvature
+	};
+	cout << "TestCL: Binding data" << endl;
+	CLtest.BindData((void *)&dataParams);
+	cout << "TestCL: Starting run" << endl;
+	CLtest.Run();
+	cout << "TestCL: done" << endl;
 }
-
-
 
